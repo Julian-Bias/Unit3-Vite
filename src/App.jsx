3333053f5bda4f00def1d78b855a5d07639fafdd
Julian Bias <julian_bias@yahoo.com>
2024-09-26 09:32:19 -0500

    added functionality and rendering puppy Data

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,42 @@ import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setfeatPupId] = useState(null);
 
+  function handleClick(puppyId) {
+    console.log("puppy id: ", puppyId);
+    setfeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("Featured pup", featuredPup);
   console.log("puppyList: ", puppyList);
+  console.log("Featured Puppy ID: ", featPupId);
 
   return (
     <>
       <div className="App">
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                handleClick(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
       </div>
     </>
